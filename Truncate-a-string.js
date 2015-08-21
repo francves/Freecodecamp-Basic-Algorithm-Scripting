fdd3d4610dd5ae9477cba4b09636c8e9888261be
francves <francves1711@gmail.com>
2015-08-21 11:23:50 -0430

    Create Truncate-a-string.js

---
+++ Truncate-a-string.js
@@ -0,0 +1,9 @@
+function truncate(str, num) {
+  // Clear out that junk in your trunk
+  if(str.length > num)
+  return str.slice(0,num-3) + "...";
+  else
+    return str;
+}
+
+truncate('A-tisket a-tasket A green and yellow basket', 11);
